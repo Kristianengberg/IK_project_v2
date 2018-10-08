f592633020a7623228bb9a879270cb771a1a582c
Kristian Engberg <krissoengberg@gmail.com>
2018-10-08 09:51:05 +0200

    .....

---
+++ Assets/Kristian/Scripts/IKConcept.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKConcept : MonoBehaviour {
+
+    public Transform child;
+    public float maxRange = 2.5f;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var heading = child.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+
+        if (heading.sqrMagnitude > maxRange * maxRange)
+        {
+            Vector3 relative = transform.InverseTransformPoint(child.position);
+            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
+            transform.Rotate(0, 0, angle);
+        }
+    }
+}
